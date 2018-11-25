9373c865ff254c4a3606512657e5f6e678cdd634
Kevin Wang <rocwang0316@gmail.com>
2018-11-25 13:18:55 -0800

    Removed the async key work to allow return.

--- services/candidate/handler.js
+++ services/candidate/handler.js
@@ -11,14 +11,14 @@ if (pool === undefined) {
   });
 }
 
-module.exports.fetchCandidatesInfo = async (event, context, callback) => {
+module.exports.fetchCandidatesInfo = (event, context, callback) => {
   // This will allow us to freeze open connections to a database
   context.callbackWaitsForEmptyEventLoop = false
   const SQL = 'SELECT * FROM candidates';
   pool.query(SQL, async (err, results, fields) => {
     if (err) throw new Error(SQL, err);
     callback(null, results);
-    // Call the smart contractor to get vote amount.
+    // // Call the smart contractor to get vote amount.
     // const newCandidates = results.map(async candidate => {
     //   const votes = await BlockChainHelper.countVote(candidate.id.toString());
     //   return { ...candidate, votes: votes.toNumber() };
